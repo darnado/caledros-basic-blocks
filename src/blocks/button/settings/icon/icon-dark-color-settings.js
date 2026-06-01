c291ade1be958fdec1e3d8a4fea649608bb2d86c
David Arnado <77515359+darnado@users.noreply.github.com>
2026-05-31 22:56:08 -0500

    chore(button block): format JS file
    Formats the icon-dark-color-settings.js file to comply with
    WordPress's Javascript coding standards.

--- src/blocks/button/settings/icon/icon-dark-color-settings.js
+++ src/blocks/button/settings/icon/icon-dark-color-settings.js
@@ -1,6 +1,6 @@
 /*
  * Caledros Basic Blocks - Easy to use Gutenberg blocks
- * Copyright (C) 2025  David Arnado
+ * Copyright (C) 2025-2026  David Arnado
  * 
  * This file is part of Caledros Basic Blocks.
  * 
@@ -18,70 +18,71 @@
  * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
  */
 
-import { PanelBody, ColorPalette } from "@wordpress/components";
-import { useSettings } from "@wordpress/block-editor";
-import { select } from "@wordpress/data";
-import { __ } from "@wordpress/i18n";
+import { PanelBody, ColorPalette } from '@wordpress/components';
+import { useSettings } from '@wordpress/block-editor';
+import { select } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
-export default function IconDarkColorSettings({ attributes, setAttributes }) {
-  const { buttonIconDarkColor } = attributes;
+export default function IconDarkColorSettings( { attributes, setAttributes } ) {
+	const { buttonIconDarkColor } = attributes;
 
-  const [themeJsonPalette, defaultPalette] = useSettings(
-    "color.palette",
-    "color.defaultPalette"
-  );
+	const [ themeJsonPalette, defaultPalette ] = useSettings(
+		'color.palette',
+		'color.defaultPalette'
+	);
 
-  // Define fallback color options
-  const fallbackColorOptions = themeJsonPalette.map((registeredColor) => {
-    return {
-      color: `var(--wp--preset--color--${registeredColor.slug})`,
-      name: registeredColor.name,
-    };
-  });
+	// Define fallback color options
+	const fallbackColorOptions = themeJsonPalette.map( ( registeredColor ) => {
+		return {
+			color: `var(--wp--preset--color--${ registeredColor.slug })`,
+			name: registeredColor.name,
+		};
+	} );
 
-  // Get available color palettes present in the Full Site Editor
-  const editorSettings = select("core/editor")?.getEditorSettings();
-  const editorPalette = editorSettings?.__experimentalFeatures?.color?.palette;
+	// Get available color palettes present in the Full Site Editor
+	const editorSettings = select( 'core/editor' )?.getEditorSettings();
+	const editorPalette =
+		editorSettings?.__experimentalFeatures?.color?.palette;
 
-  // Function to get the color palettes
-  const createColorOptions = (editorPalette, paletteType) => {
-    return {
-      colors: editorPalette?.[paletteType]?.map((palette) => {
-        return {
-          color: `var(--wp--preset--color--${palette.slug})`,
-          name: palette.name,
-        };
-      }),
-      name: `${paletteType}`,
-    };
-  };
+	// Function to get the color palettes
+	const createColorOptions = ( colorPalette, paletteType ) => {
+		return {
+			colors: colorPalette?.[ paletteType ]?.map( ( palette ) => {
+				return {
+					color: `var(--wp--preset--color--${ palette.slug })`,
+					name: palette.name,
+				};
+			} ),
+			name: `${ paletteType }`,
+		};
+	};
 
-  // Available color palettes
-  const defaultColors = createColorOptions(editorPalette, "default");
-  const themeColors = createColorOptions(editorPalette, "theme");
-  const customColors = createColorOptions(editorPalette, "custom");
+	// Available color palettes
+	const defaultColors = createColorOptions( editorPalette, 'default' );
+	const themeColors = createColorOptions( editorPalette, 'theme' );
+	const customColors = createColorOptions( editorPalette, 'custom' );
 
-  // Define color options for the controller
-  const colorOptions = [
-    ...(defaultPalette ? [defaultColors] : []),
-    themeColors,
-    ...(customColors.colors ? [customColors] : []),
-  ];
+	// Define color options for the controller
+	const colorOptions = [
+		...( defaultPalette ? [ defaultColors ] : [] ),
+		themeColors,
+		...( customColors.colors ? [ customColors ] : [] ),
+	];
 
-  return (
-    <PanelBody
-      title={__("Icon color (dark mode)", "caledros-basic-blocks")}
-      initialOpen={false}
-    >
-      <ColorPalette
-        colors={editorPalette ? colorOptions : fallbackColorOptions}
-        value={buttonIconDarkColor}
-        onChange={(newColor) =>
-          setAttributes({ buttonIconDarkColor: newColor })
-        }
-        enableAlpha={true}
-        clearable={false}
-      />
-    </PanelBody>
-  );
+	return (
+		<PanelBody
+			title={ __( 'Icon color (dark mode)', 'caledros-basic-blocks' ) }
+			initialOpen={ false }
+		>
+			<ColorPalette
+				colors={ editorPalette ? colorOptions : fallbackColorOptions }
+				value={ buttonIconDarkColor }
+				onChange={ ( newColor ) =>
+					setAttributes( { buttonIconDarkColor: newColor } )
+				}
+				enableAlpha={ true }
+				clearable={ false }
+			/>
+		</PanelBody>
+	);
 }
