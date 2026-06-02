46db7b9b549d187d90fe9b269775fd58d8feee87
David Arnado <77515359+darnado@users.noreply.github.com>
2026-06-02 11:08:23 -0500

    refactor(button block's content): reformat JS files
    Changes the format of several JS files in charge of controlling the
    link, text content, text color, content gap, and font size of the
    button block. Now these files comply with WordPress JS coding
    standards.

--- src/blocks/button/settings/button-text-content-settings.js
+++ src/blocks/button/settings/button-text-content-settings.js
@@ -1,6 +1,6 @@
 /*
  * Caledros Basic Blocks - Easy to use Gutenberg blocks
- * Copyright (C) 2025  David Arnado
+ * Copyright (C) 2025-2026  David Arnado
  * 
  * This file is part of Caledros Basic Blocks.
  * 
@@ -18,31 +18,34 @@
  * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
  */
 
-import { PanelBody, TextControl } from "@wordpress/components";
-import { __ } from "@wordpress/i18n";
+import { PanelBody, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
-export default function ButtonTextContentSettings({
-  attributes,
-  setAttributes,
-}) {
-  const { buttonText } = attributes;
+export default function ButtonTextContentSettings( {
+	attributes,
+	setAttributes,
+} ) {
+	const { buttonText } = attributes;
 
-  return (
-    <PanelBody
-      title={__("Button text", "caledros-basic-blocks")}
-      initialOpen={false}
-    >
-      <TextControl
-        __nextHasNoMarginBottom
-        __next40pxDefaultSize
-        help={__("Type the text for the button.", "caledros-basic-blocks")}
-        value={buttonText}
-        onChange={(newValue) => {
-          setAttributes({
-            buttonText: newValue,
-          });
-        }}
-      />
-    </PanelBody>
-  );
+	return (
+		<PanelBody
+			title={ __( 'Button text', 'caledros-basic-blocks' ) }
+			initialOpen={ false }
+		>
+			<TextControl
+				__nextHasNoMarginBottom
+				__next40pxDefaultSize
+				help={ __(
+					'Type the text for the button.',
+					'caledros-basic-blocks'
+				) }
+				value={ buttonText }
+				onChange={ ( newValue ) => {
+					setAttributes( {
+						buttonText: newValue,
+					} );
+				} }
+			/>
+		</PanelBody>
+	);
 }
--- src/blocks/button/settings/font-size-settings.js
+++ src/blocks/button/settings/font-size-settings.js
@@ -1,6 +1,6 @@
 /*
  * Caledros Basic Blocks - Easy to use Gutenberg blocks
- * Copyright (C) 2025  David Arnado
+ * Copyright (C) 2025-2026  David Arnado
  * 
  * This file is part of Caledros Basic Blocks.
  * 
@@ -19,212 +19,234 @@
  */
 
 import {
-  RangeControl,
-  ToggleControl,
-  SelectControl,
-  PanelBody,
-} from "@wordpress/components";
-import { __ } from "@wordpress/i18n";
-import { useState } from "@wordpress/element";
-import { useSettings } from "@wordpress/block-editor";
-import { select } from "@wordpress/data";
+	RangeControl,
+	ToggleControl,
+	SelectControl,
+	PanelBody,
+} from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+import { useSettings } from '@wordpress/block-editor';
+import { select } from '@wordpress/data';
 
-export default function FontSizeSettings({ attributes, setAttributes }) {
-  const { buttonFontSize } = attributes;
-  const [themeJsonFontSizes, defaultFontSizesEnabled] = useSettings(
-    "typography.fontSizes",
-    "typography.defaultFontSizes"
-  );
+export default function FontSizeSettings( { attributes, setAttributes } ) {
+	const { buttonFontSize } = attributes;
+	const [ themeJsonFontSizes, defaultFontSizesEnabled ] = useSettings(
+		'typography.fontSizes',
+		'typography.defaultFontSizes'
+	);
 
-  // Define fallback font size options
-  const fallbackFontSizeOptions = themeJsonFontSizes?.map(
-    (registeredFontSize) => {
-      return {
-        label: `${registeredFontSize.name} (${registeredFontSize.size})`,
-        value: `var(--wp--preset--font-size--${registeredFontSize.slug})`,
-      };
-    }
-  );
+	// Define fallback font size options
+	const fallbackFontSizeOptions = themeJsonFontSizes?.map(
+		( registeredFontSize ) => {
+			return {
+				label: `${ registeredFontSize.name } (${ registeredFontSize.size })`,
+				value: `var(--wp--preset--font-size--${ registeredFontSize.slug })`,
+			};
+		}
+	);
 
-  // Get available font sizes present in the Full Site Editor
-  const editorFontSizes =
-    select("core/editor")?.getEditorSettings()?.__experimentalFeatures
-      ?.typography?.fontSizes;
+	// Get available font sizes present in the Full Site Editor
+	const editorFontSizes =
+		select( 'core/editor' )?.getEditorSettings()?.__experimentalFeatures
+			?.typography?.fontSizes;
 
-  // Function to get the font sizes
-  const createFontSizeOptions = (editorFontSizes, fontSizeListType) => {
-    return editorFontSizes?.[fontSizeListType]?.map((fontSize) => {
-      return {
-        label: `${fontSize.name} (${fontSize.size})`,
-        value: `var(--wp--preset--font-size--${fontSize.slug})`,
-      };
-    });
-  };
+	// Function to get the font sizes
+	const createFontSizeOptions = ( editorFontSizesP, fontSizeListType ) => {
+		return editorFontSizesP?.[ fontSizeListType ]?.map( ( fontSize ) => {
+			return {
+				label: `${ fontSize.name } (${ fontSize.size })`,
+				value: `var(--wp--preset--font-size--${ fontSize.slug })`,
+			};
+		} );
+	};
 
-  // Available font sizes
-  const defaultFontSizes = createFontSizeOptions(editorFontSizes, "default");
-  const themeFontSizes = createFontSizeOptions(editorFontSizes, "theme");
-  const customFontSizes = createFontSizeOptions(editorFontSizes, "custom");
+	// Available font sizes
+	const defaultFontSizes = createFontSizeOptions(
+		editorFontSizes,
+		'default'
+	);
+	const themeFontSizes = createFontSizeOptions( editorFontSizes, 'theme' );
+	const customFontSizes = createFontSizeOptions( editorFontSizes, 'custom' );
 
-  // Define font size options for the controller
-  const fontSizeOptions = [
-    ...(defaultFontSizesEnabled ? [defaultFontSizes] : []),
-    themeFontSizes,
-    ...(customFontSizes && customFontSizes?.length !== 0
-      ? [customFontSizes]
-      : []),
-  ].flat();
+	// Define font size options for the controller
+	const fontSizeOptions = [
+		...( defaultFontSizesEnabled ? [ defaultFontSizes ] : [] ),
+		themeFontSizes,
+		...( customFontSizes && customFontSizes?.length !== 0
+			? [ customFontSizes ]
+			: [] ),
+	].flat();
 
-  // Recover font size presets
-  const registeredFontPresets = () => {
-    if (editorFontSizes) {
-      const defaultFontPresets =
-        defaultFontSizes?.map((registeredFontSize) => {
-          return registeredFontSize.value;
-        }) || [];
-      const themeFontPresets =
-        themeFontSizes?.map((registeredFontSize) => {
-          return registeredFontSize.value;
-        }) || [];
-      const customFontPresets =
-        customFontSizes?.map((registeredFontSize) => {
-          return registeredFontSize.value;
-        }) || [];
-      return [...defaultFontPresets, ...themeFontPresets, ...customFontPresets];
-    } else {
-      return themeJsonFontSizes?.map((registeredFontSize) => {
-        return registeredFontSize.value;
-      });
-    }
-  };
+	// Recover font size presets
+	const registeredFontPresets = () => {
+		if ( editorFontSizes ) {
+			const defaultFontPresets =
+				defaultFontSizes?.map( ( registeredFontSize ) => {
+					return registeredFontSize.value;
+				} ) || [];
+			const themeFontPresets =
+				themeFontSizes?.map( ( registeredFontSize ) => {
+					return registeredFontSize.value;
+				} ) || [];
+			const customFontPresets =
+				customFontSizes?.map( ( registeredFontSize ) => {
+					return registeredFontSize.value;
+				} ) || [];
+			return [
+				...defaultFontPresets,
+				...themeFontPresets,
+				...customFontPresets,
+			];
+		}
+		return themeJsonFontSizes?.map( ( registeredFontSize ) => {
+			return registeredFontSize.value;
+		} );
+	};
 
-  // Recover the unit used in the font size
-  const unit = buttonFontSize.replace(/[\d.]+/g, "") || "px";
-  const fontSizeUnit = ["px", "em", "rem", "vw", "vh"].includes(unit)
-    ? unit
-    : "px";
+	// Recover the unit used in the font size
+	const unit = buttonFontSize.replace( /[\d.]+/g, '' ) || 'px';
+	const fontSizeUnit = [ 'px', 'em', 'rem', 'vw', 'vh' ].includes( unit )
+		? unit
+		: 'px';
 
-  // Recover the numeric value of the font size
-  const fontSizeNumber = (unit) => {
-    if (unit === "px") {
-      return parseInt(buttonFontSize) || 0;
-    } else {
-      return parseFloat(buttonFontSize) || 0;
-    }
-  };
+	// Recover the numeric value of the font size
+	const fontSizeNumber = ( unitP ) => {
+		if ( unitP === 'px' ) {
+			return parseInt( buttonFontSize ) || 0;
+		}
+		return parseFloat( buttonFontSize ) || 0;
+	};
 
-  // Restrict maximum value for vw and vh units
-  const enforceMaxValue = (newUnit, valueNumber) => {
-    if (["vw", "vh"].includes(newUnit) && valueNumber > 100) {
-      return 100;
-    }
-    return valueNumber;
-  };
+	// Restrict maximum value for vw and vh units
+	const enforceMaxValue = ( newUnit, valueNumber ) => {
+		if ( [ 'vw', 'vh' ].includes( newUnit ) && valueNumber > 100 ) {
+			return 100;
+		}
+		return valueNumber;
+	};
 
-  // Update state for custom font size
-  const [useCustomFontSize, setUseCustomFontSize] = useState(
-    registeredFontPresets().includes(buttonFontSize) ? false : true
-  );
+	// Update state for custom font size
+	const [ useCustomFontSize, setUseCustomFontSize ] = useState(
+		registeredFontPresets().includes( buttonFontSize ) ? false : true
+	);
 
-  return (
-    <PanelBody
-      title={__("Font size", "caledros-basic-blocks")}
-      initialOpen={false}
-    >
-      <ToggleControl
-        __nextHasNoMarginBottom
-        label="Use custom font size"
-        checked={useCustomFontSize}
-        onChange={() =>
-          setUseCustomFontSize((oldUseCustomFontSize) => !oldUseCustomFontSize)
-        }
-      />
-      {!useCustomFontSize && (
-        <SelectControl
-          __next40pxDefaultSize
-          __nextHasNoMarginBottom
-          help={__("Select the font size.", "caledros-basic-blocks")}
-          value={
-            registeredFontPresets().includes(buttonFontSize)
-              ? buttonFontSize
-              : ""
-          }
-          options={[
-            {
-              disabled: true,
-              label: "Select an option",
-              value: "",
-            },
-            ...(editorFontSizes ? fontSizeOptions : fallbackFontSizeOptions),
-          ]}
-          onChange={(newFontSize) => {
-            setAttributes({ buttonFontSize: newFontSize });
-          }}
-        />
-      )}
-      {useCustomFontSize && (
-        <div className="cbb-editor__grid">
-          <RangeControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            help={__(
-              `Please select the custom font size (${fontSizeUnit}).`,
-              "caledros-basic-blocks"
-            )}
-            value={
-              registeredFontPresets().includes(buttonFontSize)
-                ? 0
-                : parseFloat(buttonFontSize)
-            }
-            max={fontSizeUnit === "vw" || fontSizeUnit === "vh" ? 100 : 200}
-            min={0}
-            step={fontSizeUnit === "px" ? 1 : 0.01}
-            onChange={(newFontSize) => {
-              setAttributes({
-                buttonFontSize: `${enforceMaxValue(
-                  fontSizeUnit,
-                  newFontSize
-                )}${fontSizeUnit}`,
-              });
-            }}
-          />
-          <SelectControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            value={fontSizeUnit}
-            options={[
-              {
-                label: "px",
-                value: "px",
-              },
-              {
-                label: "em",
-                value: "em",
-              },
-              {
-                label: "rem",
-                value: "rem",
-              },
-              {
-                label: "vw",
-                value: "vw",
-              },
-              {
-                label: "vh",
-                value: "vh",
-              },
-            ]}
-            onChange={(newUnit) => {
-              setAttributes({
-                buttonFontSize: `${enforceMaxValue(
-                  newUnit,
-                  fontSizeNumber(newUnit)
-                )}${newUnit}`,
-              });
-            }}
-          />
-        </div>
-      )}
-    </PanelBody>
-  );
+	return (
+		<PanelBody
+			title={ __( 'Font size', 'caledros-basic-blocks' ) }
+			initialOpen={ false }
+		>
+			<ToggleControl
+				__nextHasNoMarginBottom
+				label="Use custom font size"
+				checked={ useCustomFontSize }
+				onChange={ () =>
+					setUseCustomFontSize(
+						( oldUseCustomFontSize ) => ! oldUseCustomFontSize
+					)
+				}
+			/>
+			{ ! useCustomFontSize && (
+				<SelectControl
+					__next40pxDefaultSize
+					__nextHasNoMarginBottom
+					help={ __(
+						'Select the font size.',
+						'caledros-basic-blocks'
+					) }
+					value={
+						registeredFontPresets().includes( buttonFontSize )
+							? buttonFontSize
+							: ''
+					}
+					options={ [
+						{
+							disabled: true,
+							label: 'Select an option',
+							value: '',
+						},
+						...( editorFontSizes
+							? fontSizeOptions
+							: fallbackFontSizeOptions ),
+					] }
+					onChange={ ( newFontSize ) => {
+						setAttributes( { buttonFontSize: newFontSize } );
+					} }
+				/>
+			) }
+			{ useCustomFontSize && (
+				<div className="cbb-editor__grid">
+					<RangeControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ sprintf(
+							/**
+							 * translators: %s font size unit
+							 */
+							__(
+								'Please select the custom font size (%s).',
+								'caledros-basic-blocks'
+							),
+							fontSizeUnit
+						) }
+						value={
+							registeredFontPresets().includes( buttonFontSize )
+								? 0
+								: parseFloat( buttonFontSize )
+						}
+						max={
+							fontSizeUnit === 'vw' || fontSizeUnit === 'vh'
+								? 100
+								: 200
+						}
+						min={ 0 }
+						step={ fontSizeUnit === 'px' ? 1 : 0.01 }
+						onChange={ ( newFontSize ) => {
+							setAttributes( {
+								buttonFontSize: `${ enforceMaxValue(
+									fontSizeUnit,
+									newFontSize
+								) }${ fontSizeUnit }`,
+							} );
+						} }
+					/>
+					<SelectControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						value={ fontSizeUnit }
+						options={ [
+							{
+								label: 'px',
+								value: 'px',
+							},
+							{
+								label: 'em',
+								value: 'em',
+							},
+							{
+								label: 'rem',
+								value: 'rem',
+							},
+							{
+								label: 'vw',
+								value: 'vw',
+							},
+							{
+								label: 'vh',
+								value: 'vh',
+							},
+						] }
+						onChange={ ( newUnit ) => {
+							setAttributes( {
+								buttonFontSize: `${ enforceMaxValue(
+									newUnit,
+									fontSizeNumber( newUnit )
+								) }${ newUnit }`,
+							} );
+						} }
+					/>
+				</div>
+			) }
+		</PanelBody>
+	);
 }
