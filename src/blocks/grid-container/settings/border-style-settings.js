3919987bc9b549a6357c41330f71e7a94df7cce2
David Arnado <77515359+darnado@users.noreply.github.com>
2026-06-06 15:17:15 -0500

    fix(grid container block): change variable name
    Fixes a variable name in the border-light-color-settings.js file.
    It also updates the format of the JS files in charge in controlling
    the block's blur and border. These files now comply with WordPress
    coding standards.

--- src/blocks/grid-container/settings/border-style-settings.js
+++ src/blocks/grid-container/settings/border-style-settings.js
@@ -1,6 +1,6 @@
 /*
  * Caledros Basic Blocks - Easy to use Gutenberg blocks
- * Copyright (C) 2025  David Arnado
+ * Copyright (C) 2025-2026  David Arnado
  * 
  * This file is part of Caledros Basic Blocks.
  * 
@@ -18,350 +18,359 @@
  * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
  */
 
-import { PanelBody, ToggleControl, SelectControl } from "@wordpress/components";
-import { __ } from "@wordpress/i18n";
-import { useState } from "@wordpress/element";
+import { PanelBody, ToggleControl, SelectControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 
-export default function BorderStyleSettings({ attributes, setAttributes }) {
-  const { containerBorder } = attributes;
+export default function BorderStyleSettings( { attributes, setAttributes } ) {
+	const { containerBorder } = attributes;
 
-  const borderArray = containerBorder.style.split(" ");
-  const topBorder = borderArray[0] || "solid";
-  const rightBorder = borderArray[1] || "none";
-  const bottomBorder = borderArray[2] || "none";
-  const leftBorder = borderArray[3] || "none";
+	const borderArray = containerBorder.style.split( ' ' );
+	const topBorder = borderArray[ 0 ] || 'solid';
+	const rightBorder = borderArray[ 1 ] || 'none';
+	const bottomBorder = borderArray[ 2 ] || 'none';
+	const leftBorder = borderArray[ 3 ] || 'none';
 
-  const [useDifferentBorderStyles, setUseDifferentBorderStyles] = useState(
-    containerBorder.style.includes(" ") ? true : false
-  );
-  return (
-    <PanelBody
-      title={__("Border style", "caledros-basic-blocks")}
-      initialOpen={false}
-    >
-      <ToggleControl
-        __nextHasNoMarginBottom
-        label="Use different border styles for each side"
-        checked={useDifferentBorderStyles}
-        onChange={() => {
-          const newValue = !useDifferentBorderStyles;
-          setUseDifferentBorderStyles(newValue);
+	const [ useDifferentBorderStyles, setUseDifferentBorderStyles ] = useState(
+		containerBorder.style.includes( ' ' ) ? true : false
+	);
+	return (
+		<PanelBody
+			title={ __( 'Border style', 'caledros-basic-blocks' ) }
+			initialOpen={ false }
+		>
+			<ToggleControl
+				__nextHasNoMarginBottom
+				label="Use different border styles for each side"
+				checked={ useDifferentBorderStyles }
+				onChange={ () => {
+					const newValue = ! useDifferentBorderStyles;
+					setUseDifferentBorderStyles( newValue );
 
-          if (newValue) {
-            setAttributes({
-              containerBorder: {
-                ...containerBorder,
-                style: "solid none none none",
-              },
-            });
-          } else {
-            setAttributes({
-              containerBorder: { ...containerBorder, style: "solid" },
-            });
-          }
-        }}
-      />
-      {!useDifferentBorderStyles && (
-        <SelectControl
-          __next40pxDefaultSize
-          __nextHasNoMarginBottom
-          help={__("Choose the border style.", "caledros-basic-blocks")}
-          value={containerBorder.style}
-          options={[
-            {
-              disabled: true,
-              label: "Select an option",
-              value: "",
-            },
-            {
-              label: "None",
-              value: "none",
-            },
-            {
-              label: "Dotted",
-              value: "dotted",
-            },
-            {
-              label: "Dashed",
-              value: "dashed",
-            },
-            {
-              label: "Solid",
-              value: "solid",
-            },
-            {
-              label: "Double",
-              value: "double",
-            },
-            {
-              label: "Groove",
-              value: "groove",
-            },
-            {
-              label: "Ridge",
-              value: "ridge",
-            },
-            {
-              label: "Inset",
-              value: "inset",
-            },
-            {
-              label: "Outset",
-              value: "outset",
-            },
-          ]}
-          onChange={(newValue) => {
-            setAttributes({
-              containerBorder: { ...containerBorder, style: newValue },
-            });
-          }}
-        />
-      )}
+					if ( newValue ) {
+						setAttributes( {
+							containerBorder: {
+								...containerBorder,
+								style: 'solid none none none',
+							},
+						} );
+					} else {
+						setAttributes( {
+							containerBorder: {
+								...containerBorder,
+								style: 'solid',
+							},
+						} );
+					}
+				} }
+			/>
+			{ ! useDifferentBorderStyles && (
+				<SelectControl
+					__next40pxDefaultSize
+					__nextHasNoMarginBottom
+					help={ __(
+						'Choose the border style.',
+						'caledros-basic-blocks'
+					) }
+					value={ containerBorder.style }
+					options={ [
+						{
+							disabled: true,
+							label: 'Select an option',
+							value: '',
+						},
+						{
+							label: 'None',
+							value: 'none',
+						},
+						{
+							label: 'Dotted',
+							value: 'dotted',
+						},
+						{
+							label: 'Dashed',
+							value: 'dashed',
+						},
+						{
+							label: 'Solid',
+							value: 'solid',
+						},
+						{
+							label: 'Double',
+							value: 'double',
+						},
+						{
+							label: 'Groove',
+							value: 'groove',
+						},
+						{
+							label: 'Ridge',
+							value: 'ridge',
+						},
+						{
+							label: 'Inset',
+							value: 'inset',
+						},
+						{
+							label: 'Outset',
+							value: 'outset',
+						},
+					] }
+					onChange={ ( newValue ) => {
+						setAttributes( {
+							containerBorder: {
+								...containerBorder,
+								style: newValue,
+							},
+						} );
+					} }
+				/>
+			) }
 
-      {useDifferentBorderStyles && (
-        <>
-          <SelectControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            help={__(
-              "Choose the style for the top border.",
-              "caledros-basic-blocks"
-            )}
-            value={topBorder}
-            options={[
-              {
-                disabled: true,
-                label: "Select an option",
-                value: "",
-              },
-              {
-                label: "None",
-                value: "none",
-              },
-              {
-                label: "Dotted",
-                value: "dotted",
-              },
-              {
-                label: "Dashed",
-                value: "dashed",
-              },
-              {
-                label: "Solid",
-                value: "solid",
-              },
-              {
-                label: "Double",
-                value: "double",
-              },
-              {
-                label: "Groove",
-                value: "groove",
-              },
-              {
-                label: "Ridge",
-                value: "ridge",
-              },
-              {
-                label: "Inset",
-                value: "inset",
-              },
-              {
-                label: "Outset",
-                value: "outset",
-              },
-            ]}
-            onChange={(newValue) => {
-              setAttributes({
-                containerBorder: {
-                  ...containerBorder,
-                  style: `${newValue} ${rightBorder} ${bottomBorder} ${leftBorder}`,
-                },
-              });
-            }}
-          />
-          <SelectControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            help={__(
-              "Choose the style for the right border.",
-              "caledros-basic-blocks"
-            )}
-            value={rightBorder}
-            options={[
-              {
-                disabled: true,
-                label: "Select an option",
-                value: "",
-              },
-              {
-                label: "None",
-                value: "none",
-              },
-              {
-                label: "Dotted",
-                value: "dotted",
-              },
-              {
-                label: "Dashed",
-                value: "dashed",
-              },
-              {
-                label: "Solid",
-                value: "solid",
-              },
-              {
-                label: "Double",
-                value: "double",
-              },
-              {
-                label: "Groove",
-                value: "groove",
-              },
-              {
-                label: "Ridge",
-                value: "ridge",
-              },
-              {
-                label: "Inset",
-                value: "inset",
-              },
-              {
-                label: "Outset",
-                value: "outset",
-              },
-            ]}
-            onChange={(newValue) => {
-              setAttributes({
-                containerBorder: {
-                  ...containerBorder,
-                  style: `${topBorder} ${newValue} ${bottomBorder} ${leftBorder}`,
-                },
-              });
-            }}
-          />
-          <SelectControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            help={__(
-              "Choose the style for the bottom border.",
-              "caledros-basic-blocks"
-            )}
-            value={bottomBorder}
-            options={[
-              {
-                disabled: true,
-                label: "Select an option",
-                value: "",
-              },
-              {
-                label: "None",
-                value: "none",
-              },
-              {
-                label: "Dotted",
-                value: "dotted",
-              },
-              {
-                label: "Dashed",
-                value: "dashed",
-              },
-              {
-                label: "Solid",
-                value: "solid",
-              },
-              {
-                label: "Double",
-                value: "double",
-              },
-              {
-                label: "Groove",
-                value: "groove",
-              },
-              {
-                label: "Ridge",
-                value: "ridge",
-              },
-              {
-                label: "Inset",
-                value: "inset",
-              },
-              {
-                label: "Outset",
-                value: "outset",
-              },
-            ]}
-            onChange={(newValue) => {
-              setAttributes({
-                containerBorder: {
-                  ...containerBorder,
-                  style: `${topBorder} ${rightBorder} ${newValue} ${leftBorder}`,
-                },
-              });
-            }}
-          />
-          <SelectControl
-            __next40pxDefaultSize
-            __nextHasNoMarginBottom
-            help={__(
-              "Choose the style for the left border.",
-              "caledros-basic-blocks"
-            )}
-            value={leftBorder}
-            options={[
-              {
-                disabled: true,
-                label: "Select an option",
-                value: "",
-              },
-              {
-                label: "None",
-                value: "none",
-              },
-              {
-                label: "Dotted",
-                value: "dotted",
-              },
-              {
-                label: "Dashed",
-                value: "dashed",
-              },
-              {
-                label: "Solid",
-                value: "solid",
-              },
-              {
-                label: "Double",
-                value: "double",
-              },
-              {
-                label: "Groove",
-                value: "groove",
-              },
-              {
-                label: "Ridge",
-                value: "ridge",
-              },
-              {
-                label: "Inset",
-                value: "inset",
-              },
-              {
-                label: "Outset",
-                value: "outset",
-              },
-            ]}
-            onChange={(newValue) => {
-              setAttributes({
-                containerBorder: {
-                  ...containerBorder,
-                  style: `${topBorder} ${rightBorder} ${bottomBorder} ${newValue}`,
-                },
-              });
-            }}
-          />
-        </>
-      )}
-    </PanelBody>
-  );
+			{ useDifferentBorderStyles && (
+				<>
+					<SelectControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ __(
+							'Choose the style for the top border.',
+							'caledros-basic-blocks'
+						) }
+						value={ topBorder }
+						options={ [
+							{
+								disabled: true,
+								label: 'Select an option',
+								value: '',
+							},
+							{
+								label: 'None',
+								value: 'none',
+							},
+							{
+								label: 'Dotted',
+								value: 'dotted',
+							},
+							{
+								label: 'Dashed',
+								value: 'dashed',
+							},
+							{
+								label: 'Solid',
+								value: 'solid',
+							},
+							{
+								label: 'Double',
+								value: 'double',
+							},
+							{
+								label: 'Groove',
+								value: 'groove',
+							},
+							{
+								label: 'Ridge',
+								value: 'ridge',
+							},
+							{
+								label: 'Inset',
+								value: 'inset',
+							},
+							{
+								label: 'Outset',
+								value: 'outset',
+							},
+						] }
+						onChange={ ( newValue ) => {
+							setAttributes( {
+								containerBorder: {
+									...containerBorder,
+									style: `${ newValue } ${ rightBorder } ${ bottomBorder } ${ leftBorder }`,
+								},
+							} );
+						} }
+					/>
+					<SelectControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ __(
+							'Choose the style for the right border.',
+							'caledros-basic-blocks'
+						) }
+						value={ rightBorder }
+						options={ [
+							{
+								disabled: true,
+								label: 'Select an option',
+								value: '',
+							},
+							{
+								label: 'None',
+								value: 'none',
+							},
+							{
+								label: 'Dotted',
+								value: 'dotted',
+							},
+							{
+								label: 'Dashed',
+								value: 'dashed',
+							},
+							{
+								label: 'Solid',
+								value: 'solid',
+							},
+							{
+								label: 'Double',
+								value: 'double',
+							},
+							{
+								label: 'Groove',
+								value: 'groove',
+							},
+							{
+								label: 'Ridge',
+								value: 'ridge',
+							},
+							{
+								label: 'Inset',
+								value: 'inset',
+							},
+							{
+								label: 'Outset',
+								value: 'outset',
+							},
+						] }
+						onChange={ ( newValue ) => {
+							setAttributes( {
+								containerBorder: {
+									...containerBorder,
+									style: `${ topBorder } ${ newValue } ${ bottomBorder } ${ leftBorder }`,
+								},
+							} );
+						} }
+					/>
+					<SelectControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ __(
+							'Choose the style for the bottom border.',
+							'caledros-basic-blocks'
+						) }
+						value={ bottomBorder }
+						options={ [
+							{
+								disabled: true,
+								label: 'Select an option',
+								value: '',
+							},
+							{
+								label: 'None',
+								value: 'none',
+							},
+							{
+								label: 'Dotted',
+								value: 'dotted',
+							},
+							{
+								label: 'Dashed',
+								value: 'dashed',
+							},
+							{
+								label: 'Solid',
+								value: 'solid',
+							},
+							{
+								label: 'Double',
+								value: 'double',
+							},
+							{
+								label: 'Groove',
+								value: 'groove',
+							},
+							{
+								label: 'Ridge',
+								value: 'ridge',
+							},
+							{
+								label: 'Inset',
+								value: 'inset',
+							},
+							{
+								label: 'Outset',
+								value: 'outset',
+							},
+						] }
+						onChange={ ( newValue ) => {
+							setAttributes( {
+								containerBorder: {
+									...containerBorder,
+									style: `${ topBorder } ${ rightBorder } ${ newValue } ${ leftBorder }`,
+								},
+							} );
+						} }
+					/>
+					<SelectControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ __(
+							'Choose the style for the left border.',
+							'caledros-basic-blocks'
+						) }
+						value={ leftBorder }
+						options={ [
+							{
+								disabled: true,
+								label: 'Select an option',
+								value: '',
+							},
+							{
+								label: 'None',
+								value: 'none',
+							},
+							{
+								label: 'Dotted',
+								value: 'dotted',
+							},
+							{
+								label: 'Dashed',
+								value: 'dashed',
+							},
+							{
+								label: 'Solid',
+								value: 'solid',
+							},
+							{
+								label: 'Double',
+								value: 'double',
+							},
+							{
+								label: 'Groove',
+								value: 'groove',
+							},
+							{
+								label: 'Ridge',
+								value: 'ridge',
+							},
+							{
+								label: 'Inset',
+								value: 'inset',
+							},
+							{
+								label: 'Outset',
+								value: 'outset',
+							},
+						] }
+						onChange={ ( newValue ) => {
+							setAttributes( {
+								containerBorder: {
+									...containerBorder,
+									style: `${ topBorder } ${ rightBorder } ${ bottomBorder } ${ newValue }`,
+								},
+							} );
+						} }
+					/>
+				</>
+			) }
+		</PanelBody>
+	);
 }
