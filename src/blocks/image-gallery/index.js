559c5eaad3f4ec6ffda6b1f91dbac7d5720da16e
David Arnado <77515359+darnado@users.noreply.github.com>
2026-06-07 10:06:55 -0500

    refactor(image gallery block): reformats files
    Modifies the format of the edit-block.js, index.js, and view.js files.
    These files now follow WordPress JS coding standards. It also updates
    the copyright year in the block's CSS file. Additionally, it fixes a
    typo in the license file of the Swiper library.

--- src/blocks/image-gallery/index.js
+++ src/blocks/image-gallery/index.js
@@ -1,6 +1,6 @@
 /*
  * Caledros Basic Blocks - Easy to use Gutenberg blocks
- * Copyright (C) 2025  David Arnado
+ * Copyright (C) 2025-2026  David Arnado
  * 
  * This file is part of Caledros Basic Blocks.
  * 
@@ -18,14 +18,13 @@
  * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
  */
 
-import { registerBlockType } from "@wordpress/blocks";
-import metadata from "./block.json";
-import { __ } from "@wordpress/i18n";
-import EditBlock from "./edit-block";
-import blocksIcon from "../../assets/blocks-icon";
-import "./style.css";
+import { registerBlockType } from '@wordpress/blocks';
+import metadata from './block.json';
+import EditBlock from './edit-block';
+import blocksIcon from '../../assets/blocks-icon';
+import './style.css';
 
-registerBlockType(metadata.name, {
-  icon: blocksIcon.icon,
-  edit: EditBlock,
-});
+registerBlockType( metadata.name, {
+	icon: blocksIcon.icon,
+	edit: EditBlock,
+} );
