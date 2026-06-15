408e32a7f1c986bbf041f5bd909ea9b5e0e54234
David Arnado <77515359+darnado@users.noreply.github.com>
2026-06-14 19:55:55 -0500

    feat(posts loop block): add new controls
    Adds new typography and letter spacing controls. These controls are
    not visible in the editor nor the frontend.

---
+++ src/blocks/posts-loop/settings/letter-spacing-settings.js
@@ -0,0 +1,141 @@
+/*
+ * Caledros Basic Blocks - Easy to use Gutenberg blocks
+ * Copyright (C) 2025-2026  David Arnado
+ * 
+ * This file is part of Caledros Basic Blocks.
+ * 
+ * Caledros Basic Blocks is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * Caledros Basic Blocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {
+	PanelBody,
+	RangeControl,
+	ToggleControl,
+	SelectControl,
+} from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+
+export default function LetterSpacingSettings( { attributes, setAttributes } ) {
+	const { letterSpacing } = attributes;
+	const [ useNormalLetterSpacing, setUseNormalLetterSpacing ] = useState(
+		letterSpacing === 'normal' ? true : false
+	);
+
+	// Recover the unit used in the letter spacing
+	const unit = letterSpacing.replace( /[\d.]+/g, '' ) || 'px';
+	const letterSpacingUnit = [ 'px', 'em', 'rem' ].includes( unit )
+		? unit
+		: 'px';
+
+	// Recover the numeric value of the letter spacing
+	const letterSpacingNumber = ( unitP ) => {
+		if ( unitP === 'px' ) {
+			return parseInt( letterSpacing ) || 0;
+		}
+		return parseFloat( letterSpacing ) || 0;
+	};
+
+	// Enforce max value for pixels
+	const enforceMaxValue = ( unitP, valueNumber ) => {
+		if ( unitP !== 'px' && valueNumber > 10 ) {
+			return 10;
+		}
+		return valueNumber;
+	};
+
+	return (
+		<PanelBody
+			title={ __( 'Letter spacing', 'caledros-basic-blocks' ) }
+			initialOpen={ false }
+		>
+			<ToggleControl
+				__nextHasNoMarginBottom
+				label={ __(
+					'Use normal letter spacing',
+					'caledros-basic-blocks'
+				) }
+				checked={ useNormalLetterSpacing }
+				onChange={ () => {
+					const temporalValue = useNormalLetterSpacing
+						? '0'
+						: 'normal';
+					setUseNormalLetterSpacing( ( oldValue ) => ! oldValue );
+					setAttributes( {
+						letterSpacing: `${ temporalValue }${
+							temporalValue === 'normal' ? '' : letterSpacingUnit
+						}`,
+					} );
+				} }
+			/>
+			{ ! useNormalLetterSpacing && (
+				<div className="cbb-editor__grid">
+					<RangeControl
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						help={ sprintf(
+							/**
+							 * translators: %s letter spacing unit
+							 */
+							__(
+								'Please select the custom letter spacing (%s).',
+								'caledros-basic-blocks'
+							),
+							letterSpacingUnit
+						) }
+						value={ parseFloat( letterSpacing ) || 0 }
+						max={ letterSpacingUnit === 'px' ? 50 : 10 }
+						min={ 0 }
+						step={ letterSpacingUnit === 'px' ? 1 : 0.01 }
+						onChange={ ( newLetterSpacing ) => {
+							setAttributes( {
+								letterSpacing: `${ enforceMaxValue(
+									letterSpacingUnit,
+									newLetterSpacing
+								) }${ letterSpacingUnit }`,
+							} );
+						} }
+					/>
+					<SelectControl
+						__nextHasNoMarginBottom
+						__next40pxDefaultSize
+						value={ letterSpacingUnit }
+						options={ [
+							{
+								label: 'px',
+								value: 'px',
+							},
+							{
+								label: 'em',
+								value: 'em',
+							},
+							{
+								label: 'rem',
+								value: 'rem',
+							},
+						] }
+						onChange={ ( newUnit ) => {
+							setAttributes( {
+								letterSpacing: `${ enforceMaxValue(
+									newUnit,
+									letterSpacingNumber( newUnit )
+								) }${ newUnit }`,
+							} );
+						} }
+					/>
+				</div>
+			) }
+		</PanelBody>
+	);
+}
---
+++ src/blocks/posts-loop/settings/typography/font-style-settings.js
@@ -0,0 +1,83 @@
+/*
+ * Caledros Basic Blocks - Easy to use Gutenberg blocks
+ * Copyright (C) 2025-2026  David Arnado
+ * 
+ * This file is part of Caledros Basic Blocks.
+ * 
+ * Caledros Basic Blocks is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * Caledros Basic Blocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with Caledros Basic Blocks; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { SelectControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+export default function FontStyleSettings( {
+	attributes,
+	setAttributes,
+	getAvailableFontStyles,
+	getAvailableFontWeights,
+	doesFontExist,
+} ) {
+	const { fontFamily, fontStyle, fontWeight } = attributes;
+
+	// Default font styles
+	const defaultFontStyles = [
+		{ label: 'Normal', value: 'normal' },
+		{ label: 'Italic', value: 'italic' },
+	];
+
+	// Create options array for the controller
+	const fontStyleOptions = getAvailableFontStyles( fontFamily )?.map(
+		( styleValue ) => {
+			return {
+				label: `${ styleValue[ 0 ].toUpperCase() }${ styleValue.slice(
+					1
+				) }`,
+				value: `${ styleValue }`,
+			};
+		}
+	);
+
+	return (
+		<SelectControl
+			__next40pxDefaultSize
+			__nextHasNoMarginBottom
+			help={ __( 'Select the font style.', 'caledros-basic-blocks' ) }
+			value={ fontStyle }
+			options={
+				fontStyleOptions.length === 0
+					? defaultFontStyles
+					: fontStyleOptions
+			}
+			onChange={ ( newFontStyle ) => {
+				const availableFontWeights = doesFontExist( fontFamily )
+					? getAvailableFontWeights( fontFamily, newFontStyle )
+					: [];
+
+				const newFontWeight = availableFontWeights.includes(
+					fontWeight
+				)
+					? fontWeight
+					: availableFontWeights[ 0 ];
+
+				setAttributes( {
+					fontStyle: newFontStyle,
+					...( doesFontExist( fontFamily ) &&
+						fontWeight !== newFontWeight && {
+							fontWeight: newFontWeight,
+						} ),
+				} );
+			} }
+		/>
+	);
+}
